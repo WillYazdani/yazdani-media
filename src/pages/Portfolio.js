ba6912f215074d3a1e2e6de43744ff3d232368f1
Will Yazdani <yazd0006@gmail.com>
2022-10-08 15:03:20 -0500

    removed grid

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,66 +1,38 @@
 import "../CSS/portfolio.css";
 // import {Link} from "react-router-dom";
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
+const images = [
+    require('../assets/images/1.jpg'),
+    require('../assets/images/2.jpg'),
+    require('../assets/images/3.jpg'),
+    require('../assets/images/4.jpg'),
+    require('../assets/images/5.jpg'),
+    require('../assets/images/6.jpg'),
+    require('../assets/images/7.jpg'),
+    require('../assets/images/8.jpg'),
+    require('../assets/images/9.jpg'),
+];
 
 export default function Portfolio() {
 
     return (
         <>
-            <ResponsiveMasonry
-            className="gallery"
-            columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
-            >
-                <Masonry
-                className="images"
-                gutter="1rem"
-                >
-                    <img
-                    src={require('../assets/images/1.jpg')}
-                    alt="1"
-                    />
 
-                    <img
-                    src={require('../assets/images/2.jpg')}
-                    alt="2"
-                    />
+            {images.map((image, index) => (
+                
+                <img
+                className="gallery-image"
+                key={index}
+                src={image}
+                alt=""
+                style= {{
+                    width: "100%",
+                }}
+                 />
+                
+            ))}
 
-                    <img
-                    src={require('../assets/images/3.jpg')}
-                    alt="3"
-                    />
 
-                    <img
-                    src={require('../assets/images/4.jpg')}
-                    alt="4"
-                    />
-
-                    <img
-                    src={require('../assets/images/5.jpg')}
-                    alt="5"
-                    />
-
-                    <img
-                    src={require('../assets/images/6.jpg')}
-                    alt="6"
-                    />
-
-                    <img
-                    src={require('../assets/images/7.jpg')}
-                    alt="7"
-                    />
-
-                    <img
-                    src={require('../assets/images/8.jpg')}
-                    alt="8"
-                    />
-
-                    <img
-                    src={require('../assets/images/9.jpg')}
-                    alt="9"
-                    />
-                </Masonry>
-            </ResponsiveMasonry>
         </>
     )
 }
